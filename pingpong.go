db4a9e5a834824e5d3e15fcf2d3aef537cdaec88
anuragjha <toanuragjha@gmail.com>
2021-05-30 22:22:45 -0700

    Completed pingpong

---
+++ pingpong.go
@@ -0,0 +1,78 @@
+//  Followed from webpage - https://docs.libp2p.io/tutorials/getting-started/go/
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+	"github.com/multiformats/go-multiaddr"
+	"os"
+	"os/signal"
+	"syscall"
+)
+import "github.com/libp2p/go-libp2p"
+
+func main() {
+	// create a background context (i.e. one that never cancels)
+	ctx := context.Background()
+
+	// start a libp2p node that listens on a random local TCP port,
+	// but without running the built-in ping protocol
+	node, err := libp2p.New(ctx,
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+		libp2p.Ping(false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// configure our own ping protocol
+	pingService := &ping.PingService{Host: node}
+	node.SetStreamHandler(ping.ID, pingService.PingHandler)
+
+	// print the node's PeerInfo in multiaddr format
+	peerInfo := peer.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("libp2p node address: %s\n", addrs[0])
+
+	// if a remote peer has been passed on the command line, connect to it
+	// and send it 5 ping messages, otherwise wait for a signal to stop
+	if len(os.Args) > 1 {
+		addr, err := multiaddr.NewMultiaddr(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		aPeer, err := peer.AddrInfoFromP2pAddr(addr)
+		if err != nil {
+			panic(err)
+		}
+		if err := node.Connect(ctx, *aPeer); err != nil {
+			panic(err)
+		}
+		fmt.Println("sending 5 ping messages to", addr)
+		ch := pingService.Ping(ctx, aPeer.ID)
+		for i := 0; i < 5; i++ {
+			res := <-ch
+			fmt.Println("pinged", addr, "in", res.RTT)
+		}
+	} else {
+		// wait for a SIGINT or SIGTERM signal
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		msg := <-ch
+		fmt.Printf("Received signal: %s, shutting down...", msg)
+	}
+
+	// shut the node down
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+
+}
